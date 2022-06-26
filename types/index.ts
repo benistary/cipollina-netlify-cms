4b191e9962bcff666ccd68a88e98b26ac27d5ae9
Benjamín Starý <stary.benjamin@gmail.com>
2022-06-26 22:01:01 +0200

    change types and data

--- types/index.ts
+++ types/index.ts
@@ -1,15 +1,49 @@
-export type Homepage = {
-  category: {
+export type Pizza = {
+  pizza: {
     label: string;
     description: string;
-    products: Products;
+    products: PizzaProducts;
   }[];
 };
 
-export type Products = {
+export type PizzaProducts = {
   label: string;
   weight?: number;
   description: string;
   price: number;
   number: number;
 }[];
+
+export type Posuchy = {
+  label: string;
+  weight: number;
+  products: {
+    label: string;
+    price: number;
+  }[];
+};
+
+export type Panuozzo = {
+  panuozzo: {
+    label: string;
+    weight?: number;
+    description: string;
+    price: number;
+  };
+};
+
+export type Sticks = {
+  label: string;
+  pieces: number;
+  products: {
+    label: string;
+    price: number;
+  };
+};
+
+export type Additions = {
+  additions: {
+    label: string;
+    price: number;
+  };
+};
