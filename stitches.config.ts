316215ddef430bced88f7f3d8535d9d72fc2922d
Benjamín Starý <stary.benjamin@gmail.com>
2022-07-01 21:34:35 +0200

    ubuntu

--- stitches.config.ts
+++ stitches.config.ts
@@ -26,7 +26,7 @@ const config = {
       montserrat: "Montserrat",
       merriweather: "Merriweather",
       cormorantGaramond: "'Cormorant Garamond', serif",
-      syne: "'Fira Sans', sans-serif;",
+      syne: "'Ubuntu', sans-serif",
     },
     colors: {
       primary: "#BE312B",
